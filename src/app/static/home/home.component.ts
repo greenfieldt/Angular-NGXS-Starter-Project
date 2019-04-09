3cad968a2c6a187b3aea30b3ad146bdfad08fc75
Timothy Greenfield <timg@increatesoftware.com>
2019-04-09 13:36:26 -0600

    Clean up some issues on iPhone including one related to animations using fixed positioning

--- src/app/static/home/home.component.ts
+++ src/app/static/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Inject, Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef, PLATFORM_ID } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
 import { themes } from '../../shared/state/settings.state'
 import { Observable } from 'rxjs';
@@ -8,16 +8,9 @@ import { NotificationService } from '../../shared/notifications/notification.ser
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../shared/animations/route.animations'
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { isPlatformBrowser } from '@angular/common';
 
 
-export interface RatesQueryResponse {
-    rates: Rate[];
-}
-
-type Rate = {
-    currency: string;
-    rate: number;
-}
 
 @Component({
     selector: 'increate-home',
@@ -27,46 +20,44 @@ type Rate = {
 
 })
 export class HomeComponent implements OnInit {
+    @ViewChild('belowTheFold') btf: ElementRef;
+
+
     routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
-
-
-    rates$: Observable<any[]>;
-    loading$: Observable<boolean>;
-    errors$: Observable<any>;
-
-    themes = themes;
-
-
-    @Select(state => state.settings.theme) theme$: Observable<string>;
-
 
 
 
     constructor(private apollo: Apollo,
         private store: Store,
-        private notification: NotificationService) { }
+        private notification: NotificationService,
+        @Inject(PLATFORM_ID) private platformId) {
 
-    ngOnInit() {
-        //console.log("Home ngOnInit");
-        const source$ = this.apollo.query<RatesQueryResponse>({
-            query: gql`
-        {
-          rates(currency: "USD") {
-            currency
-            rate
-          }
-        }
-      `
-        }).pipe(shareReplay(1));
 
-        this.rates$ = source$.pipe(map((result) => result.data && result.data.rates));
-        this.loading$ = source$.pipe(map(result => result.loading));
-        this.errors$ = source$.pipe(map(result => result.errors));
+
     }
 
-    onThemeSelect($event) {
-        this.store.dispatch(new ChangeTheme($event.value));
-        this.notification.info("Theme Changed");
+
+    public loadScript() {
+        if (isPlatformBrowser(this.platformId)) {
+
+            let body = <HTMLDivElement>document.body;
+            let script = document.createElement('script');
+            script.innerHTML = '';
+            script.src = 'assets/news-app-dcfdf98018818e301e7c.js';
+            script.async = true;
+            script.defer = true;
+            body.appendChild(script);
+        }
+    }
+
+    ngOnInit() {
+        if (isPlatformBrowser(this.platformId)) {
+
+            //this.loadScript();
+            //console.log("btf:", this.btf);
+            //var child = document.createElement('<news-source> </news-source>');
+            //this.btf.nativeElement.append(child);
+        }
     }
 
 }
