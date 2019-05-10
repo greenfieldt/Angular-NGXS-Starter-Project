c5af94f51fcf64a02ce3e78c88f3d3149f38b2fc
Timothy Greenfield <timg@increatesoftware.com>
2019-05-10 11:01:14 -0600

    Login form V1

---
+++ src/app/static/modal-container/modal-container.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoginComponent } from '../login/login.component';
+
+
+@Component({
+    selector: 'app-modal-container',
+    templateUrl: './modal-container.component.html',
+    styleUrls: ['./modal-container.component.scss']
+})
+export class ModalContainerComponent implements OnInit {
+
+    sub: Subscription = new Subscription();
+
+
+    dialogRef;
+
+    constructor(private dialog: MatDialog, private route: ActivatedRoute, private router: Router) {
+        this.sub.add(route.params.pipe(
+            tap((x) => {
+                //console.log(x);
+                if (x.component === 'login') {
+                    this.dialogRef = this.dialog.open(
+                        LoginComponent, { width: '400px' });
+                }
+            })
+        ).subscribe());
+
+        this.dialogRef.afterClosed().subscribe(() => {
+            router.navigateByUrl('/');
+        });
+    }
+
+    ngOnInit() {
+
+    }
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+}
