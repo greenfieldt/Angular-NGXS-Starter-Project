ccb24a40f649e460f1c0322de4a876f9820a2526
Timothy Greenfield <timg@increatesoftware.com>
2019-05-10 12:36:53 -0600

    Added forgot password

--- src/app/static/modal-container/modal-container.component.ts
+++ src/app/static/modal-container/modal-container.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { LoginComponent } from '../login/login.component';
+import { PasswordComponent } from '../password/password.component';
 
 
 @Component({
@@ -21,16 +22,23 @@ export class ModalContainerComponent implements OnInit {
     constructor(private dialog: MatDialog, private route: ActivatedRoute, private router: Router) {
         this.sub.add(route.params.pipe(
             tap((x) => {
+                if (this.dialogRef) {
+                    this.dialogRef.close();
+                    this.dialogRef = null;
+                }
                 console.log(x);
                 if (x.component === 'login') {
                     this.dialogRef = this.dialog.open(
                         LoginComponent, { width: '400px' });
+                } else if (x.component === 'forgotpassword') {
+                    this.dialogRef = this.dialog.open(
+                        PasswordComponent, { width: '400px' });
                 }
             })
         ).subscribe());
 
         this.dialogRef.afterClosed().subscribe(() => {
-            router.navigateByUrl('/');
+            //router.navigateByUrl('/');
         });
     }
 
--- src/app/static/password/password.component.ts
+++ src/app/static/password/password.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { Observable, timer } from 'rxjs';
+import { tap, take, filter, debounceTime } from 'rxjs/operators'
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../shared/animations/route.animations';
+import { Store } from '@ngxs/store';
+import { EmailLoginResetPassword } from 'src/app/shared/state/auth.actions';
+
+
+export interface PasswordResetForm {
+    email: string;
+}
+
 
 @Component({
-  selector: 'app-password',
-  templateUrl: './password.component.html',
-  styleUrls: ['./password.component.scss']
+    selector: 'app-password',
+    templateUrl: './password.component.html',
+    styleUrls: ['./password.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class PasswordComponent implements OnInit {
+    loginFailed = false;
+    loginErrorMessage = "";
+    routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
 
-  constructor() { }
+    form = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+    });
 
-  ngOnInit() {
-  }
 
+    constructor(private fb: FormBuilder,
+        private matDialog: MatDialog,
+        private store: Store,
+        private changeDetRef: ChangeDetectorRef) { }
+
+    ngOnInit() {
+    }
+
+    resetPassword() {
+        this.store.dispatch(
+            new EmailLoginResetPassword(this.form.get('email').value,
+                ((err: any) => {
+                    console.log(err);
+                    this.loginErrorMessage = err.message;
+                    this.loginFailed = true;
+                    this.changeDetRef.detectChanges();
+
+                    timer(5000).pipe(
+                        tap(_ => {
+                            this.loginErrorMessage = '';
+                            this.loginFailed = false;
+                            this.changeDetRef.detectChanges();
+                        }),
+                        take(1)
+                    ).subscribe();
+                })
+            ));
+    }
+
+
+    close() {
+        this.matDialog.closeAll();
+    }
 }
+
